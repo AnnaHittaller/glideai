76d21b77be4b264f832e2265c66e135e67afdb0a
Anna Hittaller <anna.hittaller@brandung.de>
2026-02-24 10:08:04 +0100

    initial setup

---
+++ app/slices/RichText/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Content } from "@prismicio/client";
+
+defineProps(getSliceComponentProps<Content.RichTextSlice>());
+</script>
+
+<template>
+  <section>
+    <PrismicRichText :field="slice.primary.content" />
+  </section>
+</template>
+
+<style scoped>
+section {
+  max-width: 600px;
+  margin: 6em auto;
+}
+
+section:deep(.codespan) {
+  font-family: monospace;
+}
+</style>
